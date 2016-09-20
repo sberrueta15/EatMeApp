a70f46badcfb280ea46c4c52c596172b37db2b0a
santi_berrueta15@hotmail.com <sberrueta@alutel.com.uy>
2016-09-20 19:06:17 -0300

    First Commit

---
+++ EatMeApp/EatMeApp/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EatMeApp.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ EatMeApp/EatMeApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EatMeApp.WebApiApplication" Language="C#" %>
