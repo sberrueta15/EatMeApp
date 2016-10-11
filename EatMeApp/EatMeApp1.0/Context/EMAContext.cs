db0da0c28913d8882c37ba74a921e5cb6606227d
hardyrz <hardyr.93@gmail.com>
2016-10-11 20:52:48 -0300

    New Project.

---
+++ EatMeApp/EatMeApp1.0/Context/EMAContext.cs
@@ -0,0 +1,20 @@
+﻿using EatMeApp1._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EatMeApp1._0.Context
+{
+    public class EMAContext : DbContext
+    {
+        public EMAContext()
+        {
+            Database.SetInitializer<EMAContext>(new CreateDatabaseIfNotExists<EMAContext>());
+
+        }
+
+        
+    }
+}
---
+++ EatMeApp/EatMeApp1.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EatMeApp1._0.WebApiApplication" Language="C#" %>
