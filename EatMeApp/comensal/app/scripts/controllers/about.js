5548a2af2c663cb3dbddfb2e58c8531c155d2ff6
toti1212 <rodrigosrz12@gmail.com>
2016-11-08 19:14:00 -0300

    Agregada aplicacion de comensal

---
+++ EatMeApp/comensal/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comensalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the comensalApp
+ */
+angular.module('comensalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ EatMeApp/comensal/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name comensalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the comensalApp
+ */
+angular.module('comensalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
