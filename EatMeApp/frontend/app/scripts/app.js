5fb2cbdae83d66b19d3bfaadcfba888dd0b8e7e2
toti1212 <rodrigosrz12@gmail.com>
2016-11-08 19:34:18 -0300

    agregada la home
    Agregada la Home

--- EatMeApp/frontend/app/scripts/app.js
+++ EatMeApp/frontend/app/scripts/app.js
@@ -8,73 +8,95 @@
  *
  * Main module of the application.
  */
-angular
-  .module('EatMeApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ui.router',
-    'ui.bootstrap',
-    'textAngular',
-    'uiGmapgoogle-maps',
-    'ngGeolocation',
+ angular
+ .module('EatMeApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'ui.router',
+  'ui.bootstrap',
+  'textAngular',
+  'uiGmapgoogle-maps',
+  'ngGeolocation',
 
   ])
-  .config(['$stateProvider','$urlRouterProvider','uiGmapGoogleMapApiProvider',
-    function ($stateProvider,$urlRouterProvider,GoogleMapApiProviders) {
+ .config(['$stateProvider','$urlRouterProvider','uiGmapGoogleMapApiProvider',
+  function ($stateProvider,$urlRouterProvider,GoogleMapApiProviders) {
 
 
 
-      $stateProvider
-      .state('chef',{
-        url:'/chef',
-        templateUrl:'views/base.html',
-        controller:'BaseCtrl',
-        controllerAs:'base',
-        abstract:true,
-      })
+    $stateProvider
+    .state('home',{
+      url:'/home',
+      views:{
+        'home':{
+          templateUrl:'views/home.html',
+        },
+      },
+    })
 
-      .state('chef.crear-evento',{
-        url:'/crear-evento',
-        templateUrl:'views/evento.crear.html',
-        controller:'EventoCtrl',
-        controllerAs:'evento',
-      })
+    .state('chef',{
+      url:'/chef',
+      templateUrl:'views/base.html',
+      controller:'BaseCtrl',
+      controllerAs:'base',
+      abstract:true,
+    })
+
+    .state('chef.crear-evento',{
+      url:'/crear-evento',
+      templateUrl:'views/evento.crear.html',
+      controller:'EventoCtrl',
+      controllerAs:'evento',
+    })
 
 
-      .state('chef.evento-detalle',{
-       url:':id/detalle-evento/',
-         params:{id:null,eventoObj:null},
-        templateUrl:'views/evento.detalle.html',
-        controller:'EventoDetalleCtrl',
-        controllerAs:'eventoDetalle',
-      })
+    .state('chef.evento-detalle',{
+     url:':id/detalle-evento/',
+     params:{id:null,eventoObj:null},
+     templateUrl:'views/evento.detalle.html',
+     controller:'EventoDetalleCtrl',
+     controllerAs:'eventoDetalle',
+   })
 
 
-      .state('chef.dashboard',{
-        url:'/dashboard',
-        templateUrl:'views/dashboard.html',
-        controller:'DashboardCtrl',
-        controllerAs:'dashboard',
-      })
+    .state('chef.dashboard',{
+      url:'/dashboard',
+      templateUrl:'views/dashboard.html',
+      controller:'DashboardCtrl',
+      controllerAs:'dashboard',
+    })
 
-      .state('chef.mi-perfil',{
-        url:'/mi-perfil',
-        templateUrl:'views/mi-perfil.html',
-        controller:'MiperfilCtrl',
-        controllerAs:'perfil',
-      })
+    .state('chef.mi-perfil',{
+      url:'/mi-perfil',
+      templateUrl:'views/mi-perfil.html',
+      controller:'MiperfilCtrl',
+      controllerAs:'perfil',
+    })
 
-      $urlRouterProvider.otherwise('/chef/crear-evento');
+    $urlRouterProvider.otherwise('/chef/crear-evento');
 
-      GoogleMapApiProviders.configure({
+    GoogleMapApiProviders.configure({
           //    key: 'your api key',
           v: '3.20', //defaults to latest 3.X anyhow
           libraries: 'weather,geometry,visualization'
-      });
+        });
 
 
-  }]);
+  }])
+ .run(['$rootScope','$state',
+  function ($rootScope, $state) {
+    $rootScope.isHome = false;
+    
+    $rootScope.$on('$stateChangeStart', function (e, toState) {
+      if (toState.name === 'home') {
+        $rootScope.isHome = true;
+    } else {
+      $rootScope.isHome = false;
+    }
+  });
+
+}]);
